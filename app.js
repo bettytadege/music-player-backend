701be7c5492342c34c002210ae0b29b0fd60862b
bettytadege <bettytadege13@gmail.com>
2025-06-05 09:59:17 +0300

    refactor:add user route on available end point and update the other to

--- app.js
+++ app.js
@@ -29,13 +29,32 @@ app.get('/', (req, res) => {
     <
     <h3>Available Endpoints:</h3>
     <ul>
-      <li>GET /api/playlists - Get all playlists</li>
-      <li>POST /api/playlists - Create a playlist</li>
-      <li>GET /api/playlists/:id - Get a playlist by ID</li>
-      <li>PUT /api/playlists/:id - Update a playlist</li>
+    // user
+    <li>GET /api/users - Get all users</li>
+    <li>POST /api/user/register - Get all users</li>
+    <li>POST /api/login - Get all users</li>
+    <li>POST /api/gorget - Get all users</li>
+    // playlist
+      <li>GET /api/v1/playlist-for-you - Get all playlists</li>
+      <li>POST /api/v1/playlist-for-you - Create a playlist</li>
+      <li>GET /api/v1/playlist-for-you/:id - Get a playlist by ID</li>
+      <li>PUT /api/v1/playlist-for-you/:id - Update a playlist</li>
       <li>DELETE /api/playlists/:id - Delete a playlist</li>
+    // most listend
+       <li>GET /api/v1/most-listened' - get allmost listend song</li>
+
+      <li>GET /api/v1/most-listened'/:id - Get a most listend song by ID</li>
+      <li>PUT /api/v1/most-listened'/:id - Update a most listend song</li>
+      <li>DELETE /api/v1/most-listened'/:id - Delete  most listend song</li>
+
+      // track of the week
+      <li>GET //api/v1/track-of-the-week - Get all track of the week</li>
+      <li>POST /api/v1/track-of-the-week- Create  track of the week</li>
+      <li>GET /api/v1/track-of-the-week/:id - Get track of the week by ID</li>
+      <li>PUT /api/v1/track-of-the-week/:id - Update track of the week</li>
+      <li>DELETE /api/v1/track-of-the-week/:id - Delete  track of the week</li>
     </ul>
-    <p>Hosted on Render: <a href="https://musicplayer-backend-fduc.onrender.com" target="_blank">Go to API</a></p>
+    
   `);
 });
 
